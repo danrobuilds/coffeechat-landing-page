8d74a2c99c53ae4a69d074c90cc4af2bdb88a75b
danrobuilds <danielleerosario@gmail.com>
2025-01-13 15:47:47 -0800

    wrod roate

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ export const Hero = () => {
               by connecting with brilliant people.
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-            Coffeechat helps you network lightning fast by connecting you to individuals with complementary skillsets and experiences to yours.            </p>
+            Coffeechat helps you network lightning fast by connecting you to individuals with complementary skillsets and experiences.            </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               {/* 
